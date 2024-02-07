eabc82076a5b922c2f0a2eabdfa5298862dd1cd7
Dimitar Ivanov <ivanov.dimitur.00@gmail.com>
2024-02-07 14:36:13 +0200

    Added country entity. Added country controller. Added ApplicationDbContext. Made a migration.

--- Cookbook.web/Data/Migrations/20240207121315_AddCountriesTableToDatabase.Designer.cs
+++ Cookbook.web/Data/Migrations/20240207121315_AddCountriesTableToDatabase.Designer.cs
@@ -4,6 +4,7 @@ using Cookbook.web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cookbook.web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240207121315_AddCountriesTableToDatabase")]
+    partial class AddCountriesTableToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Cookbook.web/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cookbook.web.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            Id = Guid.NewGuid().ToString().Substring(0, 7);
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
