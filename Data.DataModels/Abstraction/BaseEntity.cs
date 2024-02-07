3e5e85a2f99f18abee92a2b83658b53c91f5e926
Dimitar Ivanov <ivanov.dimitur.00@gmail.com>
2024-02-07 16:01:20 +0200

    Added BaseEntity to Abstraction and IBase Entity to Interfaces. Added IAuditInfo to Interfaces. Added PromotionStatus to Enums.

---
+++ Data.DataModels/Abstraction/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.DataModels.Interfaces;
+
+namespace Data.DataModels.Abstraction
+{
+    public class BaseEntity : IBaseEntity<string>, IAuditInfo
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid().ToString().Substring(0, 7);
+        }
+
+        public string Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
