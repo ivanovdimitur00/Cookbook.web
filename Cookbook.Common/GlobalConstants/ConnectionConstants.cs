b5c22190e307acf17b01e8a80aeb830dcaa2e77f
Dimitar Ivanov <ivanov.dimitur.00@gmail.com>
2024-02-08 17:03:50 +0200

    Added the project.Common class library. Created the connection constants and moved them in the global constants folder. fixed the connectionconstant dependencies in applicationDbContext @ Data.DataAccess

---
+++ Cookbook.Common/GlobalConstants/ConnectionConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Cookbook.Common.GlobalConstants
+{
+    public class ConnectionConstants
+    {
+        public const string DatabaseConnectionString = @"C:\Users\User\AppData\Roaming\Microsoft\UserSecrets" +
+           @"\aspnet-Cookbook.Web-0562f6e0-f469-41dd-b124-74d205d848e2";
+
+        public const string SecretsJSONFileName = "secrets.json";
+
+        public const string SecretsJSONConnectionStringSection = "DefaultConnection:ConnectionString";
+    }
+}
