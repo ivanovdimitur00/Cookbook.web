6a3af0008f4d75304ef7ccfb3887318691af066f
Dimitar Ivanov <ivanov.dimitur.00@gmail.com>
2024-02-08 17:32:12 +0200

    Configured the ConnectionConstants. Added pathing to the user secrets file.

--- Cookbook.Common/GlobalConstants/ConnectionConstants.cs
+++ Cookbook.Common/GlobalConstants/ConnectionConstants.cs
@@ -2,8 +2,8 @@
 {
     public class ConnectionConstants
     {
-        public const string DatabaseConnectionString = @"C:\Users\User\AppData\Roaming\Microsoft\UserSecrets" +
-           @"\aspnet-Cookbook.Web-0562f6e0-f469-41dd-b124-74d205d848e2";
+        public const string DatabaseConnectionString = @"C:\Users\Dimitar\AppData\Roaming\Microsoft\UserSecrets" +
+           @"\aspnet-Cookbook.web-7DAADB50-DF1B-4E48-BD4E-77084800B430";
 
         public const string SecretsJSONFileName = "secrets.json";
 
