4611b23d4a0ab1bea7b2c34dd0323f879efd53c1
Dimitar Ivanov <ivanov.dimitur.00@gmail.com>
2024-02-09 14:29:05 +0200

    created a folder "Helpers"in project commons. added PageResultsEnum and Paginated lists to support pagination

---
+++ Cookbook.Common/Helpers/PageResultsEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SubtitlesManagementSystem.Common.Helpers
+{
+    public enum PageResultsEnum
+    {
+        [Display(Name = "3")]
+        ThreeResults = 3,
+        [Display(Name = "5")]
+        FiveResults = 5,
+        [Display(Name = "7")]
+        SevenResults = 7,
+        [Display(Name = "10")]
+        TenResults = 10
+    }
+}
